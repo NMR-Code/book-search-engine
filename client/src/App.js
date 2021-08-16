49668d3e23c883ce15f185cb21f462183f21f33c
Nelson Rosado <nelsonog827@gmail.com>
2021-08-16 11:21:27 -0400

    updated README.md with deployed app link

--- client/src/App.js
+++ client/src/App.js
@@ -7,33 +7,34 @@ import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
 const client = new ApolloClient({
-  request: operation => {
-    const token = localStorage.getItem('id_token');
-    operation.setContext({
-      headers: {
-        authorization: token ? `Bearer ${token}` : ''
-      }
-    });
-  },
-  uri: '/graphql'
+	request: (operation) => {
+		const token = localStorage.getItem('id_token');
+		operation.setContext({
+			headers: {
+				authorization: token ? `Bearer ${token}` : '',
+			},
+		});
+	},
+	uri: '/graphql',
 });
 
-
 function App() {
-  return (
-    <ApolloProvider client={client}>
-      <Router>
-        <>
-          <Navbar />
-          <Switch>
-            <Route exact path='/' component={SearchBooks} />
-            <Route exact path='/saved' component={SavedBooks} />
-            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-          </Switch>
-        </>
-      </Router>
-    </ApolloProvider>
-  );
+	return (
+		<ApolloProvider client={client}>
+			<Router>
+				<>
+					<Navbar />
+					<Switch>
+						<Route exact path='/' component={SearchBooks} />
+						<Route exact path='/saved' component={SavedBooks} />
+						<Route
+							render={() => <h1 className='display-2'>Wrong page!</h1>}
+						/>
+					</Switch>
+				</>
+			</Router>
+		</ApolloProvider>
+	);
 }
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -17,9 +17,9 @@ const PORT = process.env.PORT || 3001;
 
 // Set up the Apollo Server and pass in the schema
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware
+	typeDefs,
+	resolvers,
+	context: authMiddleware,
 });
 
 // integrate the Apollo server with the Express application as middleware
@@ -31,17 +31,19 @@ app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+	app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 // GraphQL and Express Server start
 db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-  });
+	app.listen(PORT, () => {
+		console.log(`API server running on port ${PORT}!`);
+		console.log(
+			`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+		);
+	});
 });
